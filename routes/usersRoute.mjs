dcc2f61cd5589446fcf382256f271890e8b98ae6
Jonatangithub <tyster11@outlook.com>
2024-01-23 10:44:33 +0100

    random id

--- routes/usersRoute.mjs
+++ routes/usersRoute.mjs
@@ -1,9 +1,25 @@
 import express, { response } from "express";
 import User from "../modules/user.mjs";
 import HttpCodes from "../modules/httpErrorCodes.mjs";
-
-
 const USER_API = express.Router();
+
+
+
+function generateRandomId(length) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let randomId = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * alphabet.length);
+        randomId += alphabet.charAt(randomIndex);
+    }
+
+    return randomId;
+}
+
+// Usage:
+let generatedId = generateRandomId(5);
+console.log(generatedId);
 
 const users = [];
 
@@ -15,6 +31,7 @@ USER_API.get('/:id', (req, res) => {
     /// TODO: 
     // Return user object
 })
+
 USER_API.get('/', (req, res) => {
 res.status(HttpCodes.SuccesfullRespons.Ok).send(users).end();
 })
@@ -30,18 +47,19 @@ USER_API.post('/', (req, res, next) => {
         const user = new User();
         user.name = name;
         user.email = email;
+        user.id = generateRandomId(5);
         console.log(users);
         ///TODO: Do not save passwords.
         user.pswHash = password;
+        const exists = users.some(user => user.email === email);
 
         ///TODO: Does the user exist?
-        let exists = false;
 
         if (!exists) {
             users.push(user);
             res.status(HttpCodes.SuccesfullRespons.Ok).end();
         } else {
-            res.status(HttpCodes.ClientSideErrorRespons.BadRequest).end();
+            res.status(HttpCodes.ClientSideErrorRespons.BadRequest).send("bruker eksisterer allerede").end();
         }
 
     } else {
